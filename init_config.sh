2ad3131301476c346005566cf05fbad37536d004
KC <bikecheng@gmail.com>
2021-01-16 08:22:29 +0800

    correct an issue
    copy plug.vim but not the whole directory

--- init_config.sh
+++ init_config.sh
@@ -125,7 +125,7 @@ function init_vim {
   #  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   git clone https://github.com/junegunn/vim-plug.git /tmp/vim-plug
   mkdir -p ~/.vim/autoload
-  cp /tmp/vim-plug ~/.vim/autoload/plug.vim
+  cp /tmp/vim-plug/plug.vim ~/.vim/autoload/plug.vim
   cp $SCRIPT_DIR/conf/vimrc.vim-plug ~/.vimrc
 
   # Vista supports lsp which covers tags related showcase
